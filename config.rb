6ae2fa9ecebf21c9aee6e5080407fc52eb0cc0ea
Artur Gurgul <artur.gurgul@gmail.com>
2021-10-20 12:59:53 +0200

    add old styles

--- config.rb
+++ config.rb
@@ -8,4 +8,7 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
 set :build_dir, 'docs'
